121985d1bec1285d7bdef472b25d158e2037bc86
SeokHoL <sukho8407@gmail.com>
2024-04-18 16:28:41 +0900

    직접 풀어보기 6-1

--- app/src/main/java/kr/ac/kopo/reservation2004/MainActivity.kt
+++ app/src/main/java/kr/ac/kopo/reservation2004/MainActivity.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.CalendarView
 import android.widget.Chronometer
+import android.widget.DatePicker
 import android.widget.RadioGroup
 import android.widget.RadioGroup.OnCheckedChangeListener
 import android.widget.TextView
@@ -18,7 +19,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var  btnStart : Button
     lateinit var  btnDone : Button
     lateinit var  rg : RadioGroup
-    lateinit var  calendar : CalendarView
+    lateinit var  calendar : DatePicker
     lateinit var  timePick : TimePicker
     lateinit var  textResult : TextView
     var selecteddYear :Int = 0
@@ -34,7 +35,7 @@ class MainActivity : AppCompatActivity() {
         btnStart=findViewById<Button>(R.id.btnStart)
         btnDone=findViewById<Button>(R.id.btinDone)
         rg =findViewById<RadioGroup>(R.id.rg)
-        calendar = findViewById<CalendarView>(R.id.calender)
+        calendar = findViewById<DatePicker>(R.id.calender)
         timePick = findViewById<TimePicker>(R.id.timePick)
         textResult = findViewById(R.id.textResult)
 
@@ -50,16 +51,20 @@ class MainActivity : AppCompatActivity() {
         btnDone.setOnClickListener {
             chrono.stop()
             chrono.setTextColor(Color.CYAN)
+            selecteddYear = calendar.year
+            selectedMonth = calendar.month
+            selectedDay = calendar.dayOfMonth
+
             textResult.setText(""+ selecteddYear + "년" + selectedMonth + "월" + selectedDay + "일")
             textResult.append("" + timePick.currentHour + "시")
             textResult.append("" + timePick.currentMinute + "분")
         }
 
-        calendar.setOnDateChangeListener { view, year, month, dayOfMonth ->
-            selecteddYear = year
-            selectedMonth = month + 1
-            selectedDay =dayOfMonth
-        }
+//        calendar.setOnDateChangeListener { view, year, month, dayOfMonth ->
+//            selecteddYear = year
+//            selectedMonth = month + 1
+//            selectedDay =dayOfMonth
+//        }
     }
 
     var rgListener = OnCheckedChangeListener{group,chekedId->
